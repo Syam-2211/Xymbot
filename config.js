5d127f874d060dd095c4bc23cc17f0d7ea01790c
Syam-2211 <syamsona2009@gmail.com>
2026-02-12 21:37:13 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+global.owner = ['919947121619'] // Your number
+global.ownerName = '🤍⃞𝄟ꪶ𝐒͢ʏ᪳ᴀ͓ᴍ͎ ͢𝐒ᴇ͓ꪳʀ͎𖦻⃞🍓' //
+global.botName = '🕊🦋⃝♥⃝ѕиєнα🍁♥⃝🦋⃝🕊' //
+
+// Initializing the database for your group settings
+global.db = { data: { chats: {}, users: {} } }
